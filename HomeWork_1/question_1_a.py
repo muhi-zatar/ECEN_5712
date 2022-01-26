8c71fd1884551de3f1ef08c47fccd63b80ec4f02
muhi-zatar <46324129+muhi-zatar@users.noreply.github.com>
2022-01-26 11:18:27 -0700

    Update question_1_a.py

--- HomeWork_1/question_1_a.py
+++ HomeWork_1/question_1_a.py
@@ -1,1 +1,27 @@
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import collections
+
+data = pd.read_csv("Classify-3DwLabels-2.txt")
+sorted_list = data.sort_values(by=["label"])
+fig = plt.figure()
+
+ax = fig.add_subplot(projection='3d')
+
+indices = [0,109, 249]
+#import pdb;pdb.set_trace()
+for i in range(len(indices)-1):
+
+    xs = list(sorted_list["feat_1"])[indices[i]: indices[i+1]]
+    ys = list(sorted_list["feat_2"])[indices[i]: indices[i+1]]
+    zs = list(sorted_list["feat_3"])[indices[i]: indices[i+1]]
+
+    ax.scatter(xs, ys, zs)
+
+ax.set_xlabel('Feature 3')
+ax.set_ylabel('Feature 1')
+ax.set_zlabel('Feature 2')
+
+plt.show()
